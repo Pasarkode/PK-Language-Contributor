62c80a917f9be0edfe1b37e87b910d8d546387ef
Ano Van <anovanmaximuz@gmail.com>
2014-06-24 19:49:05 +0700

    update
    current update

---
+++ rss.php
@@ -0,0 +1,12 @@
+<?php
+//RSS
+$rss_title_update_everytime          = 'Update everytime';
+$rss_title_update_everytime_text     = 'Easy access update pasarkode.com, we provide some RSS categorized :';
+$rss_general                         = 'General';
+$rss_source_code                     = 'Source Code';
+$rss_configuration                   = 'Configuration';
+$rss_configuration_text              = 'Keep up to date of Source Code in Pasarkode.com, we use RSS v.2.0';
+
+
+
+?> 
